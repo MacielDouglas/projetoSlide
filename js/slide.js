9053e7e647aaf117556f9f3de1d60354510d2516
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-01-23 08:49:09 -0300

    calculo de distancia quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -1,23 +1,37 @@
-export class Slide {
+export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
 
     this.wrapper = document.querySelector(wrapper)
+    this.dist = {
+      finalPosition: 0, startX: 0, movement: 0
+    }
+  }
+
+  moveSlide(distX)  {
+    this.dist.movePosition = distX; //guardar uma referencia da distancia
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+   return this.dist.finalPosition - this.dist.movement;
   }
 
   onStar(event) {
     event.preventDefault(); // para previnir o padrão e não bugar
-    console.log("mouseDown")
-    this.wrapper.addEventListener("mousemove", this.onMove)
+    this.dist.startX = event.clientX;
+    this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
   onMove(event) {
-    console.log("moveu")
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
-    console.log('acabou')
     this.wrapper.removeEventListener("mousemove", this.onMove)
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
